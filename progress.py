1aca218903cd0653bde2be386f7b5f0fa0a6bbfd
Katie Hughes <katiereeshughes@gmail.com>
2023-04-17 11:05:30 -0500

    pacign plots

--- progress.py
+++ progress.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 days = []
 miles = []
 total_miles_lst = []
 total_miles = 0
+
+pacing = []
+
+# parse file
 
 with open('progress.txt') as f:
     for l in f.readlines():
@@ -17,11 +22,25 @@ with open('progress.txt') as f:
         total_miles += dist
         total_miles_lst.append(total_miles)
 
+        day_pacing = []
+        for i in range(dist):
+            try:
+                mile_time = spl[i+2]
+                mile_time_spl = mile_time.split(':')
+                minutes = int(mile_time_spl[0])
+                seconds = int(mile_time_spl[1])
+                pace_in_seconds = minutes*60 + seconds
+                # print(f"Minutes: {minutes}, seconds: {seconds}, total {pace_in_seconds}")
+                day_pacing.append(pace_in_seconds)
+            except:
+                pass
+        pacing.append(day_pacing)
+
 maximum = max(total_miles_lst+days)
 
 # Plot Cumulative Miles
 
-plot_cumulative = True
+plot_cumulative = False
 
 if plot_cumulative:
     plt.plot((1,maximum),(1, maximum), color='b', label='Goal')
@@ -38,7 +57,7 @@ if plot_cumulative:
 
 # Plot difference from ideal
 
-plot_difference = True
+plot_difference = False
 
 if plot_difference:
     diff = []
@@ -55,4 +74,32 @@ if plot_difference:
     plt.ylabel('# Miles Away from Goal')
     plt.legend()
     plt.savefig('delta-miles')
+    plt.show()
+
+# Plot Average Pace
+
+plot_pacing = True
+avg_pace = []
+avg_pace_mins = []
+fastest_pace = []
+fastest_pace_mins = []
+if plot_pacing:
+    for i in range(0, len(days)):
+        avg_pace.append(np.mean(pacing[i]))
+        avg_pace_mins.append(avg_pace[i]/60.0)
+        fastest_pace.append(min(pacing[i]))
+        fastest_pace_mins.append(fastest_pace[i]/60.0)
+        # mins = int(avg_pace[i]//60)
+        # secs = avg_pace[i] - mins*60
+        # print(f"Avg Pace Day {days[i]}: {avg_pace[i]} {mins}:{secs}")
+    plt.scatter(days, avg_pace_mins)
+    plt.xlabel('Days of 2023')
+    plt.ylabel('Avg Pace (mins/mile)')
+    plt.savefig("average-pacing")
+    plt.show()
+
+    plt.scatter(days, fastest_pace_mins)
+    plt.xlabel('Days of 2023')
+    plt.ylabel('Fastest Mile (mins/mile)')
+    plt.savefig("fastest-pacing")
     plt.show()
