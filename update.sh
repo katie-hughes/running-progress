5e4f34c99dfb8d8c5cacbdea903cce5600428ed0
Katie Hughes <katiereeshughes@gmail.com>
2023-08-13 20:59:46 -0400

    Update for 08/13/2023

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 todays_date=$(date +'%m/%d/%Y')
-echo Updating for $todays_date
+# echo Updating for $todays_date
 python3 progress.py
 git add --a
-git commit -m "update for $todays_date"
+git commit -m "Update for $todays_date"
 git status
 git push
